93016726f9b0048acc834a5562124c4a230695cc
DamianParejko <damian.parejko@gmail.com>
2021-03-05 22:33:34 +0100

    Edit Post Model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,9 +5,10 @@ namespace App\Models;
 use App\Models\User;
 use App\Models\Comment;
 use App\Models\LikePost;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\DatabaseNotification;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Post extends Model
 {
@@ -35,16 +36,18 @@ class Post extends Model
         return $this->user_id === $user->id;
     }
 
-    public function isNotAuthor(User $user){
-        return $this->user_id != $user->id;
-    }
-
     public function like_post(){
         return $this->hasMany(LikePost::class);
     }
 
     public function likedBy(User $user) {
         return $this->like_post->contains('user_id', $user->id);
+    }
+
+    public function hasLike(Post $post){
+        return $this->like_post
+                ->where('post_id', $post->id)
+                ->where('user_id', Auth::user()->id);
     }
 
     public function countLike(){
@@ -55,7 +58,9 @@ class Post extends Model
         return $this->like_post->where('likeable_id', -1)->count();
     }
 
-    public function hasLike(Post $post){
-        return $this->like_post->where('post_id', $post->id)->where('user_id', Auth::user()->id)->count();
+    public function hasNotify(Post $post){
+        return DatabaseNotification::where('type', 'App\Notifications\NotificationPostLike')
+                ->where('data->post->id', $post->id)
+                ->where('data->user->id', Auth::user()->id);
     }
 }
