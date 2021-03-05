2cd9d18f5a0748c9d22fc303945268bf61921969
DamianParejko <damian.parejko@gmail.com>
2021-03-05 20:15:58 +0100

    Add PostLike Jobs

---
+++ app/Jobs/CreateLike.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Post;
+use App\Models\LikePost;
+use Illuminate\Bus\Queueable;
+use App\Events\PostLikeCreated;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+
+class CreateLike implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    private $post;
+
+    public function __construct(Post $post)
+    {
+        $this->post = $post;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $like = new LikePost([
+            'user_id' => Auth::user()->id,
+            'post_id' => $this->post->id,
+            'likeable_id' => 1
+        ]);
+
+        $like->save();
+
+        event(new PostLikeCreated($this->post));
+
+        return $like;
+    }
+}
---
+++ app/Jobs/DeleteLike.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Post;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+
+class DeleteLike implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    private $post;
+
+    public function __construct(Post $post)
+    {
+        $this->post = $post;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $this->post->hasLike($this->post)->first->delete();
+    }
+}
